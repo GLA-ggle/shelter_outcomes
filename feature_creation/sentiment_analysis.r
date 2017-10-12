380eb000206b15c010967e36959704ab0ade791d
Anna Gibson <Anna.Gibson@london.gov.uk>
2017-10-12 17:34:37 +0100

    added sentiment analysis R script

---
+++ feature_creation/sentiment_analysis.r
@@ -0,0 +1,34 @@
+
+#load libraries required
+
+library ('RSentiment') #library used for sentiment analysis
+library('plyr')
+library('dplyr')
+library('ggplot2')
+
+#read in csv
+train <- read.csv('M:\\Python Scripts\\Kaggle\\shelter-outcomes\\data\\train.csv')
+
+#calculate frequency of unique names (perhaps names could be tidied prior to this step)
+names<- count(train["Name"])
+
+#look at 'names'
+head(names)
+
+#using RSentiment, calculate score (numerical and descriptive) for each unique name
+names.sentiment <- cbind(names,calculate_score(names$Name),calculate_sentiment(names$Name))
+
+#look at result
+head(names.sentiment)
+
+#subset to keep columns required
+names.sentiment<-names.sentiment[,c("Name","calculate_score(names$Name)","sentiment")]
+
+#rename columns to something more sensible
+sentiment<- plyr::rename(names.sentiment, c("calculate_score(names$Name)"="sentiment_score","sentiment"="sentiment_desc" ))
+
+#Join to original table to assign sentiment score to each animal id
+animal.name.sentiment <- merge(x = train[c(1,2)], y = sentiment, by = "Name", all.x = TRUE  )
+
+#write to new variables folder in GitHub repo
+write.csv(animal.name.sentiment, file = "M:\\GitHub\\shelter_outcomes\\new_variables\\sentiment.csv")
